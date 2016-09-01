e899b10dada6284e0fbdd3ad0777c0a1bba1d564
Tim Foley <thetimfoley@gmail.com>
2016-09-01 13:01:54 -0400

    Used js to generate cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,22 @@ var card1 = "king";
 var card2 = "queen";
 var card3 = "king";
 var card4 = "queen";
+
+var board = document.getElementById('game-board');
+
+var createBoard = function() {
+  for (i = 0; i < 4; i++) {
+    //create blank div
+    var cardTemplate = document.createElement('div');
+
+    //give it the class 'card'
+    cardTemplate.className = 'card';
+
+    //append to board
+    board.appendChild(cardTemplate);
+
+  }
+
+}
+
+createBoard();
